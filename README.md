a816f121fead8bee7b57e852af98411c120f2182
Sirine Dridi <sirine@live.unc.edu>
2021-02-28 22:38:33 -0500

    a3 template

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# A3-BST
-comp 210 spring21 binary search tree assignment
+## A3: Binary Search Tree
+
+Your task for this assignment is to implement the methods in BSTImpl.java. The descriptions of the methods are provided 
+in the BST interface.
+
+You should not need to import any packages. A main class is provided for you to test your code. 
---
+++ src/a3/BSTImpl.java
@@ -0,0 +1,66 @@
+package a3;
+
+public class BSTImpl implements BST {
+
+    private Node root;
+    private int size;
+
+    public BSTImpl() {
+        root = null;
+        size = 0;
+    }
+
+    // The implementation of "height" is given to you below
+    // it is here to illustrate for you how to set up
+    // the method implementation as recursion.
+    // You should follow this pattern for implementing the other recursive methods
+    // by adding your own private recursive methods
+
+    @Override
+    public int height() { // public interface method signature
+        // this method is the public interface method
+        // it is not recursive, but it calls a recursive
+        // private method and passes it access to the tree cells
+        return height_recursive(this.root);
+    }
+    private int height_recursive(Node c) {
+        // inner method with different signature
+        // this helper method uses recursion to traverse
+        // and process the recursive structure of the tree of cells
+        if (c==null) return -1;
+        int lht = height_recursive(c.getLeft());
+        int rht = height_recursive(c.getRight());
+        return Math.max(lht,rht) + 1;
+    }
+
+    @Override
+    public Node getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public String insert(String value) { return null; }
+
+    @Override
+    public void remove(String value) { }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public String findMin() {
+        return null;
+    }
+
+    @Override
+    public String findMax() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+}
